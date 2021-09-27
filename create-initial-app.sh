b4fe146450e2d27f6baa50134b5543f4fba2f4a9
Kevin Obee <kevin.obee@gmail.com>
2021-09-27 11:45:20 +0100

    use yamls folder for bootstrapping applications

--- create-initial-app.sh
+++ create-initial-app.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 echo "Create Argo CD application from Git repository"
-argocd app create applications --repo https://github.com/kevinobee/gitops-bootstrap.git --path apps --dest-server https://kubernetes.default.svc --dest-namespace default --upsert --revision apps --self-heal --sync-policy automated --sync-option Prune=true --auto-prune
+argocd app create applications --repo https://github.com/kevinobee/gitops-bootstrap.git --path yamls --dest-server https://kubernetes.default.svc --dest-namespace default --upsert --revision apps --self-heal --sync-policy automated --sync-option Prune=true --auto-prune
