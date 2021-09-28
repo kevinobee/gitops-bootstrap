ac26a931f2c41437e79b63e48b99636da59a04b3
Kevin Obee <kevin.obee@gmail.com>
2021-09-28 09:00:59 +0100

    add helm repositories before iapplications install

--- create-initial-app.sh
+++ create-initial-app.sh
@@ -2,12 +2,12 @@
 
 set -eo pipefail
 
-echo "Create Argo CD application from Git repository"
-argocd app create applications --repo https://github.com/kevinobee/gitops-bootstrap.git --path yamls --dest-server https://kubernetes.default.svc --dest-namespace default --upsert --revision apps --self-heal --sync-policy automated --sync-option Prune=true --auto-prune
-
 echo "Install Helm repositories"
 argocd repo add https://charts.helm.sh/stable --type helm --name stable
 argocd repo add https://charts.bitnami.com/bitnami  --type helm --name bitnami
 argocd repo add https://prometheus-community.github.io/helm-charts --type helm --name prometheus-community
 argocd repo add https://charts.appscode.com/stable/ --type helm --name appscode
 argocd repo add https://bitnami-labs.github.io/sealed-secrets --type helm --name sealed-secrets
+
+echo "Create Argo CD application from Git repository"
+argocd app create applications --repo https://github.com/kevinobee/gitops-bootstrap.git --path yamls --dest-server https://kubernetes.default.svc --dest-namespace default --upsert --revision apps --self-heal --sync-policy automated --sync-option Prune=true --auto-prune
