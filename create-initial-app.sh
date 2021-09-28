732f2872f887877eba0bf60dcab35effc48cb8f3
Kevin Obee <kevin.obee@gmail.com>
2021-09-28 07:39:55 +0100

    add helm repos to argo-cd

--- create-initial-app.sh
+++ create-initial-app.sh
@@ -4,3 +4,7 @@ set -eo pipefail
 
 echo "Create Argo CD application from Git repository"
 argocd app create applications --repo https://github.com/kevinobee/gitops-bootstrap.git --path yamls --dest-server https://kubernetes.default.svc --dest-namespace default --upsert --revision apps --self-heal --sync-policy automated --sync-option Prune=true --auto-prune
+
+echo "Install Helm repositories"
+argocd repo add https://charts.helm.sh/stable --type helm --name stable
+argocd repo add https://prometheus-community.github.io/helm-charts --type helm --name prometheus-community
